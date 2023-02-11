60c952ec016f8ac2966772f3604cefdeb5cfc881
Nick Bolton <nick@nbolton.com>
2023-02-11 10:22:42 +0000

    Generated PNGs

--- hardware/a1200-psu/a1200-psu-png.bat
+++ hardware/a1200-psu/a1200-psu-png.bat
@@ -1,6 +1,8 @@
 @rem Converts a PDF to a PNG
-@set file=a1200-psu
+@set file1=a1200-psu
+@set file2=a1200-psu-pcb
 
 @where pdftoppm >nul 2>nul
 @if %ERRORLEVEL% neq 0 choco install poppler
-pdftoppm -png %file%.pdf > %file%.png
+pdftoppm -png %file1%.pdf > %file1%.png
+pdftoppm -png %file2%.pdf > %file2%.png
